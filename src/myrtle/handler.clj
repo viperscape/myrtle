f98aa0de6c6f9f4c9f0baf87895c066e13a19a50
viperscape <chrisfgill@gmail.com>
2014-02-15 10:11:21 -0500

    Update handler.clj
    super basic file handling ability

--- src/myrtle/handler.clj
+++ src/myrtle/handler.clj
@@ -12,7 +12,10 @@
       [clojure.data.json :as json]
       [clojure.tools.nrepl :as nrepl]
       [clojure.tools.nrepl.server :as nreplserver]
-      [clojure.tools.nrepl.middleware :as nreplmiddle]))
+      [clojure.tools.nrepl.middleware :as nreplmiddle]
+      [clojure.java.io :as io]
+      [me.raynes.fs :as fs]))
+
 
 
 (def buffers (ref {}))
@@ -39,29 +42,44 @@
 
 
 (defn async-handler [request]
-  (println "connection from " (:remote-addr request))
- (if (or (.contains "192.168.2" (:remote-addr request)) (= "127.0.0.1" (:remote-addr request)))
-  (try (with-channel request channel
-  	   (on-close channel (fn [status] (println channel " channel closed: " status)))
+ (println "connection from " (:remote-addr request) "; " (.startsWith (:remote-addr request) "192.168.2"))
+ (if (.startsWith (:remote-addr request) "192.168.2")
+  (with-channel request channel
+    (on-close channel (fn [status] (println channel " channel closed: " status)))
     (if (websocket? channel)
+      
     	(on-receive channel (fn [data] 
         (let [req (json/read-str data :key-fn keyword)]
           (prn channel " in-channel data: " req)
 
-          (if-let [buff (:buffer req)]
-            (do ;;it'd be nice to do this in a case block instead
-              (if (:open buff) (if-not (= "" (:open buff))
+          (if-let [buff (:buffer req)] ;;note 'open file as buffer' expects file to exist, not really put thru paces either
+            (do ;; these should be in a switch case block
+              (if (:open buff) (if-not (clojure.string/blank? (:open buff))
                 (if-let [b ((keyword (:open buff)) @buffers)]
-                  (send! channel (json/write-str {:console (str "opening buffer " b)}))
-                  (send! channel (json/write-str {:console (str "opening file as buffer " buff)})))
+                  (send! channel (json/write-str {:console (str "opening buffer " b)})))
+                 ; (send! channel (json/write-str {:console (str "opening file as buffer " buff),
+                 ;                                 :file {:contents (slurp buff),:extension (fs/extension d),:filename d}})))
                 (send! channel (json/write-str {:buffer (new-buffer channel)}))))
               (if (:save buff) (send! channel (json/write-str {:console "saving buffer"})))
               (if (:kill buff) (send! channel (json/write-str {:console "killing buffer"})))
               (if (:list buff) (send! channel (json/write-str {:console (str "buffer list: " (keys @buffers))})))
               ))
 
-          ;(if-let [f (:file req)]
-           ; (if (:list f) )
+
+           (if-let [f (:file req)]
+            (if-let [d (:list f)]
+              (let [d (or (if-not (clojure.string/blank? d) d)
+                           (System/getProperty "user.dir"))] ;; if blank use home dir.
+              (send! channel (json/write-str {:file 
+                                              (if-not (fs/file? d) ;; then list dir
+                                                {:list (conj (map #(apply str d "/" %) 
+                                                                  (fs/list-dir d)) (str(fs/parent d)))}
+                                                (let [fc {:contents (slurp d),
+                                                          :extension (fs/extension d),
+                                                          :filename d}]
+                                                  (dosync (alter buffers update-in [(keyword(:buffer f)) :file] (constantly fc)))
+                                                  fc))}))
+              )))
 
           (if-let [repl-req (:repl req)] (send! channel  (json/write-str {:repl (vec (or  
             (map json/write-str(filter :value 
@@ -72,23 +90,21 @@
             (try (let [c (vec (completions (:completions req)))] (if-not (empty? c) c "nil"))
               (catch Exception e (.getMessage e))) "nil")})))
           )))
-      )) ;(send! channel (slurp "resources/main.html"))))
-  (catch Exception e (do 
-    (println (str "caught exception: " (.getMessage e)))))
-  )))
+      (send! channel (slurp "resources/buffer.html"))))
+  ))
 
 (defroutes allroutes
   (GET "/" [] async-handler)
   ;;ignore below routes! 
-  (GET "/buffers" [] (resp/file-response "resources/buffer.html"));{ch :async-channel} (str ch))
-  (GET "/buffer/:bid" {ch :async-handler {bid :bid} :params} (get-buffer ch bid)) ;;{ch :async-channel, bid :bid}
-  (GET "/buffer" {ch :async-channel} (resp/redirect (str "/buffer/" (name (new-buffer ch)))))
+  ;(GET "/buffers" [] (resp/file-response "resources/buffer.html"));{ch :async-channel} (str ch))
+  ;(GET "/buffer/:bid" {ch :async-handler {bid :bid} :params} (get-buffer ch bid)) ;;{ch :async-channel, bid :bid}
+  ;(GET "/buffer" {ch :async-channel} (resp/redirect (str "/buffer/" (name (new-buffer ch)))))
   (route/files "/" {:root "resources/public"})
   (route/resources "/")
   (route/not-found "404"))
 
 
-(defonce server (run-server (site #'allroutes) {:port 80}))
+(defonce server (run-server (site #'allroutes) {:port 8080}))
 (defonce replserver (nreplserver/start-server :port 7888))
 
 (defn -main
